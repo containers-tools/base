17e2dedbc1e701d080605c832d0ccbdc5921279d
Marek Goldmann <marek.goldmann@gmail.com>
2016-07-19 09:56:10 +0200

    Fix license headers, add LICENSE file

--- jboss/install.py
+++ jboss/install.py
@@ -1,3 +1,11 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details.
+"""
+
 from zipfile import ZipFile
 from cct.module import Module
 from cct.lib.jboss_module import JBossCliModule
--- jboss/jboss_cli.py
+++ jboss/jboss_cli.py
@@ -5,6 +5,7 @@ All rights reserved.
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details.
 """
+
 import logging
 from cct.errors import CCTError
 from cct.module import Module
@@ -13,6 +14,7 @@ import os
 import tempfile
 import shutil
 from time import sleep, time
+
 logger = logging.getLogger('cct')
 
 class JBossCliModule(Module):
